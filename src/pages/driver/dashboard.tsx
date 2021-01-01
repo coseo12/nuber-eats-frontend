991088feeb0cdb33822c4890b8fe7499f529fbee
coseo12 <momo_12@naver.com>
2021-01-01 17:44:53 +0900

    add driver dashboard

--- src/pages/driver/dashboard.tsx
+++ src/pages/driver/dashboard.tsx
@@ -1,13 +1,64 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Helmet } from 'react-helmet-async';
+import GoogleMapReact from 'google-map-react';
+
+interface ICoords {
+  lat: number;
+  lng: number;
+}
 
 export const Dashboard = () => {
+  const [driverCoords, setDriverCoords] = useState<ICoords>({ lat: 0, lng: 0 });
+  const [map, setMap] = useState<any>(null);
+  const [maps, setMaps] = useState<any>(null);
+  const onSucces: PositionCallback = ({ coords: { latitude, longitude } }) => {
+    setDriverCoords({ lat: latitude, lng: longitude });
+  };
+  const onError: PositionErrorCallback = error => {
+    alert(error);
+  };
+  const onApiLoaded = ({ map, maps }: { map: any; maps: any }) => {
+    map.panTo(new maps.LatLng(driverCoords.lat, driverCoords.lng));
+    setMap(map);
+    setMaps(maps);
+  };
+  useEffect(() => {
+    navigator.geolocation.watchPosition(onSucces, onError, {
+      enableHighAccuracy: true,
+    });
+  }, []);
+  useEffect(() => {
+    if (map && maps) {
+      map.panTo(new maps.LatLng(driverCoords.lat, driverCoords.lng));
+    }
+  }, [driverCoords.lat, driverCoords.lng]);
   return (
     <div>
       <Helmet>
         <title>Dashboard | Nuber Eats</title>
       </Helmet>
-      Dashboard
+      <div
+        className="overflow-hidden"
+        style={{ width: `100vw`, height: `50vh` }}
+      >
+        <GoogleMapReact
+          yesIWantToUseGoogleMapApiInternals
+          onGoogleApiLoaded={onApiLoaded}
+          defaultZoom={16}
+          draggable={false}
+          defaultCenter={{ lat: 33, lng: 127 }}
+          bootstrapURLKeys={{ key: 'AIzaSyBM0k_VvlAs52qP7mRiTFq86EX1lhDLJc4' }}
+        >
+          <div
+            //@ts-ignore
+            lat={driverCoords.lat}
+            lng={driverCoords.lng}
+            className="text-lg"
+          >
+            🚖
+          </div>
+        </GoogleMapReact>
+      </div>
     </div>
   );
 };
