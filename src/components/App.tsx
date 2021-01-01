c15eaa736cbb72bbc5a233841fa10237d7b38de6
coseo12 <momo_12@naver.com>
2021-01-01 16:31:24 +0900

    modify App

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,8 @@
 import { useReactiveVar } from '@apollo/client';
 import React from 'react';
 import { isLoggedInVar } from '../apollo';
-import { LoggedInRouter } from '../router/logged-in-router';
-import { LoggedOutRouter } from '../router/logged-out-router';
+import { LoggedInRouter } from '../routers/logged-in-router';
+import { LoggedOutRouter } from '../routers/logged-out-router';
 
 export const App = () => {
   const isLoggedIn = useReactiveVar(isLoggedInVar);
