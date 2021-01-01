3080fc3addbcb05396bd55c8f51302959c8fa330
coseo12 <momo_12@naver.com>
2021-01-01 22:35:31 +0900

    add

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const colors = require('tailwindcss/colors');
 
 module.exports = {
-  purge: ['./src/**/*.tsx'],
+  purge: ['./src/**/*.tsx', './src/**/*.js', './src/**/*.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
