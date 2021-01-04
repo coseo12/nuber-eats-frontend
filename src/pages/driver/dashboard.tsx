e5945069f67c1a5921a93e44bfeb1f13e6a2ee64
coseo12 <momo_12@naver.com>
2021-01-04 17:45:54 +0900

    add

--- src/pages/driver/dashboard.tsx
+++ src/pages/driver/dashboard.tsx
@@ -100,7 +100,7 @@ export const Dashboard = () => {
       );
     }
   };
-  const key: string = '';
+  const key: string = 'AIzaSyBM0k_VvlAs52qP7mRiTFq86EX1lhDLJc4';
 
   const { data: cookedOrderData } = useSubscription<cookedOrders>(
     COOKED_ORDERS_SUBSCRIPTION
