3810500788752c266025695c09f1bfdb6c663e5f
coseo12 <momo_12@naver.com>
2021-01-06 18:09:43 +0900

    add

--- src/components/category-item.tsx
+++ src/components/category-item.tsx
@@ -17,7 +17,7 @@ export const CategoryItem: React.FC<ICategoryItemProps> = ({
         className={`w-16 h-16 bg-cover rounded-full opacity-${
           opacity ? opacity : 100
         }`}
-        style={{ backgroundImage: `url(${coverImg})` }}
+        style={{ backgroundImage: `url(${coverImg ? coverImg : 'https://images.unsplash.com/photo-1559941727-6fb446e7e8ae?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=2250&q=80'})` }}
       ></div>
       <span className="mt-1 text-sm text-center font-medium uppercase">
         {name}
